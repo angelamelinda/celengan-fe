1fa659d8e1bbe0a801b4506e295b46f44574b8e9
Angela Melinda <angela.melinda@gmail.com>
2020-10-19 12:35:11 +0700

    Remove logger

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import logger from 'redux-logger'
 
 import RootReducer from './reducers';
 
-const Store = createStore(RootReducer, applyMiddleware(thunkMiddleware, logger));
+const Store = createStore(RootReducer, applyMiddleware(thunkMiddleware));
 
 export default Store;
