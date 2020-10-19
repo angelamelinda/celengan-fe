843a9a25adca35f5dcb58a1214baba0ae3271b3f
Angela Melinda <angela.melinda@gmail.com>
2020-10-19 19:53:01 +0700

    set register service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Route, BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import App from "./App";
-import store from "./redux/store";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,10 +12,10 @@ ReactDOM.render(
       <Route component={App} />
     </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
